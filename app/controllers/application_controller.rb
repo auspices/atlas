6219ec3f8b3f4bfd94a8f7cd60fbfea030015ec5
dzucconi <mail@damonzucconi.com>
2014-08-04 21:26:55 -0400

    Fix auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :current_user
+  helper_method :current_user
 
   private
 
   def current_user
-    @current_user = request.env['HTTP_AUTHORIZATION'].present?
+    @current_user ||= if auth_str = request.env['HTTP_AUTHORIZATION']
+      "#{ENV['ADMIN_USERNAME']}:#{ENV['ADMIN_PASSWORD']}" == Base64.decode64(auth_str.sub(/^Basic\s+/, ''))
+    end
   end
 
   def authenticate
