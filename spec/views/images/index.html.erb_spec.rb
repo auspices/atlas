a768a679463546aa51d646a14bfbf80bcb72fe69
dzucconi <mail@damonzucconi.com>
2014-08-04 22:01:59 -0400

    Update specs

--- spec/views/images/index.html.erb_spec.rb
+++ spec/views/images/index.html.erb_spec.rb
@@ -5,9 +5,32 @@ RSpec.describe 'images/index', type: :view do
     @images = assign(:images, Kaminari.paginate_array(2.times.map { Fabricate(:image) }).page(1))
   end
 
-  it 'renders a list of images' do
-    html = render
-    expect(html.scan(/img/).length).to eql(2)
-    expect(html.scan(/a href/).length).to eql(2)
+  describe 'logged out' do
+    before(:each) do
+      allow(view).to receive(:current_user).and_return(false)
+    end
+
+    it 'renders a list of images' do
+      expect(render.scan(/img/).length).to eql(2)
+      expect(render.scan(/a href/).length).to eql(2)
+    end
+
+    it 'does not render the delete links' do
+      expect(render).to_not include('image-delete-link')
+    end
+  end
+
+  describe 'logged in' do
+    before(:each) do
+      allow(view).to receive(:current_user).and_return(true)
+    end
+
+    it 'renders the delete links' do
+      expect(render).to include('image-delete-link')
+    end
+
+    it 'renders the add link' do
+      expect(render).to include('image-add-link')
+    end
   end
 end
--- spec/views/images/show.html.erb_spec.rb
+++ spec/views/images/show.html.erb_spec.rb
@@ -6,7 +6,27 @@ RSpec.describe 'images/show', type: :view do
     @image = assign(:image, Fabricate(:image))
   end
 
-  it 'renders attributes in the .full-image wrapper' do
-    expect(render).to include('full-image')
+  describe 'logged out' do
+    before(:each) do
+      allow(view).to receive(:current_user).and_return(false)
+    end
+
+    it 'renders attributes in the .image-show-stage wrapper' do
+      expect(render).to include('image-show-stage')
+    end
+
+    it 'does not render the delete link' do
+      expect(render).to_not include('image-delete-link')
+    end
+  end
+
+  describe 'logged in' do
+    before(:each) do
+      allow(view).to receive(:current_user).and_return(true)
+    end
+
+    it 'renders the delete link' do
+      expect(render).to include('image-delete-link')
+    end
   end
 end
