25b20679289a6ea939b82b601cc7dd6df5713b72
dzucconi <mail@damonzucconi.com>
2014-08-09 20:27:03 -0400

    Adds vanity route for images#new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'logout' => 'sessions#destroy', as: 'logout'
   get 'login' => 'sessions#new', as: 'login'
   get 'register' => 'users#new', as: 'register'
+  get 'add' => 'images#new', as: 'add'
 
   resources :users do
     resources :images, except: [:edit, :update]
