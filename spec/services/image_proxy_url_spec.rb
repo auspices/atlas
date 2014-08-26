25c609bb04c25f379c66ff6f236a3bf3f81dc5fd
dzucconi <mail@damonzucconi.com>
2014-08-25 22:16:43 -0400

    Switch over to imgix for image processing

--- spec/services/image_proxy_url_spec.rb
+++ spec/services/image_proxy_url_spec.rb
@@ -1,31 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe ImageProxyUrl do
-  before(:each) do
-    allow_any_instance_of(ImageProxyUrl).to receive(:embedly_key).and_return('xxx')
-    allow_any_instance_of(ImageProxyUrl).to receive(:cloudfront_url).and_return('http://bucket.cloudfront.net')
-  end
-
   it 'raises an error when the url option is missing' do
     expect { ImageProxyUrl.new }.to raise_error('Requires :url')
   end
 
   it 'sets some sensible defaults' do
-    url = ImageProxyUrl.new(url: 'foobar')
-    expect(url.method).to eql(:resize)
-    expect(url.options[:height]).to eql(1000)
-    expect(url.options[:width]).to eql(1000)
-    expect(url.options[:quality]).to eql(95)
-    expect(url.options[:key]).to eql('xxx')
+    url = ImageProxyUrl.new(url: 'http://foobar.s3.amazonaws.com/id/image.jpg')
+    expect(url.options[:h]).to eql(1000)
+    expect(url.options[:w]).to eql(1000)
+    expect(url.options[:q]).to eql(95)
+    expect(url.options[:fit]).to eql(:max)
+    expect(url.options[:fm]).to eql(:pjpg)
   end
 
   it 'sets options and ignores keys not represented in ImageProxyUrl::QUERY_PARAMS' do
-    url = ImageProxyUrl.new(url: 'foobar', foo: 'bar')
+    url = ImageProxyUrl.new(url: 'http://foobar.s3.amazonaws.com/id/image.jpg', foo: 'bar')
     expect(url.options[:foo]).to be_nil
   end
 
   it 'returns a usuable url' do
-    url = ImageProxyUrl.new(url: 'foobar')
-    expect(url.url).to eql('http://bucket.cloudfront.net/1/image/resize?height=1000&key=xxx&quality=95&url=foobar&width=1000')
+    url = ImageProxyUrl.new(url: 'http://foobar.s3.amazonaws.com/id/image.jpg')
+    expect(url.url).to eql('http://foobar.imgix.net/id/image.jpg?fit=max&fm=pjpg&h=1000&q=95&w=1000')
   end
 end
