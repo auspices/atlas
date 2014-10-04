463d8abd6ab988755a3c7496f5deebf48c6c8c33
dzucconi <mail@damonzucconi.com>
2014-10-03 20:54:08 -0400

    Only include top level links in the root endpoint

--- app/controllers/api/v1/root_controller.rb
+++ app/controllers/api/v1/root_controller.rb
@@ -8,27 +8,7 @@ module Api
             status: { href: api_status_index_url },
             user: {
               href: CGI.unescape(api_user_url('{id}')),
-              templated: true,
-              images: {
-                href: CGI.unescape(api_user_images_url('{user_id}')),
-                templated: true
-              },
-              image: {
-                href: CGI.unescape(api_user_image_url('{user_id}', '{id}')),
-                templated: true
-              },
-              collections: {
-                href: CGI.unescape(api_user_collections_url('{user_id}')),
-                templated: true
-              },
-              collection: {
-                href: CGI.unescape(api_user_collection_url('{user_id}', '{collection_id}')),
-                templated: true,
-                images: {
-                  href: CGI.unescape(api_user_collection_images_url('{user_id}', '{collection_id}')),
-                  templated: true
-                }
-              }
+              templated: true
             }
           }
         }
