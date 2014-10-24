1feaf79fd62b1f20cbb66ed93243c04c0481d35a
dzucconi <mail@damonzucconi.com>
2014-10-24 00:00:06 -0400

    Adds width and height columns to Images

---
+++ db/migrate/20141024023453_add_dimensions_to_images.rb
@@ -0,0 +1,8 @@
+require 'open-uri'
+
+class AddDimensionsToImages < ActiveRecord::Migration
+  def change
+    add_column :images, :width, :integer
+    add_column :images, :height, :integer
+  end
+end
