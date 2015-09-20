f7414e1483055f4dbfd9af7e0ceeb5b29e5e0a09
dzucconi <mail@damonzucconi.com>
2015-09-19 21:16:11 -0400

    Implements non-paginated Array serializer

---
+++ app/serializers/api/v1/array_serializer.rb
@@ -0,0 +1,32 @@
+module Api
+  module V1
+    class ArraySerializer < BaseSerializer
+      attributes :_embedded, :_links, :total_count
+
+      def initialize(object, options = {})
+        @current_url = options[:current_url]
+        @each = options[:each_serializer]
+        @resource_name = options[:resource_name]
+        super(object, options)
+      end
+
+      def total_count
+        self.object.size
+      end
+
+      def _embedded
+        ActiveModel::ArraySerializer.new(self.object, each_serializer: @each, root: @resource_name)
+      end
+
+      def current_url(options = {})
+        @current_url.call options
+      end
+
+      def _links
+        {
+          self: { href: current_url }
+        }
+      end
+    end
+  end
+end
