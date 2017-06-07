1b467b196270f20b6ace8d74b6bd5204e1fc369f
dzucconi <mail@damonzucconi.com>
2017-06-07 00:54:27 -0400

    Adds route to prepend to images for simple adding

--- app/controllers/collections/images_controller.rb
+++ app/controllers/collections/images_controller.rb
@@ -17,10 +17,31 @@ module Collections
       end
     end
 
+    # GET /:user_id/:collection_id/add/:source_url
+    def add
+      source_url = add_params.values.join('.').gsub(':/', '://').gsub(':///', '://')
+
+      ActiveRecord::Base.transaction do
+        @collection = current_user.collections.find(params[:collection_id])
+        @image = current_user.images.create(source_url: source_url)
+        @connection = Connector.build(current_user, @collection, @image)
+        @connection.save
+      end
+      if @connection.persisted?
+        redirect_to [current_user, @collection], success: 'Image was added'
+      else
+        redirect_to [current_user, @collection], error: @image.errors.full_messages.join(', ')
+      end
+    end
+
     private
 
     def image_params
       params.require(:image).permit(:source_url)
+    end
+
+    def add_params
+      params.permit(:source_url, :format)
     end
   end
 end
