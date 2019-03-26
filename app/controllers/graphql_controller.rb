1d761a0b0632eb62ae7869c958e5f90aeb957143
dzucconi <mail@damonzucconi.com>
2019-03-26 08:42:25 -0400

    Skips login for GraphQL endpoint

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -2,6 +2,7 @@
 
 class GraphqlController < ApplicationController
   protect_from_forgery with: :null_session
+  skip_before_filter :require_login
 
   def execute
     variables = ensure_hash(params[:variables])
