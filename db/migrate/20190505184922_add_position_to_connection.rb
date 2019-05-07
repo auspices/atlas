80e2f791c7bab85159d9ae65dcf994a1bf3be96d
dzucconi <mail@damonzucconi.com>
2019-05-06 22:23:03 -0400

    Corrects sort order when migrating positions

--- db/migrate/20190505184922_add_position_to_connection.rb
+++ db/migrate/20190505184922_add_position_to_connection.rb
@@ -5,7 +5,7 @@ class AddPositionToConnection < ActiveRecord::Migration[5.2]
     add_column :connections, :position, :integer
 
     Collection.find_each do |collection|
-      collection.connections.order(:updated_at).each.with_index do |connection, index|
+      collection.connections.unscope(:order).order(created_at: :desc).each.with_index do |connection, index|
         connection.update_column(:position, index)
       end
     end
