657cdeff776e80837241988a41db20fe50f7a768
dzucconi <mail@damonzucconi.com>
2019-07-26 22:55:15 -0400

    Fleshes out seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
-User.create!(
+user = User.create!(
   username: 'neo',
   password: 'secret',
   password_confirmation: 'secret',
   email: 'neo@example.com'
 )
+
+collection = user.collections.create!(title: 'Genesis')
+
+entity = EntityBuilder.build(user: user, value: 'Hello world')
+entity.save!
+
+collection.contents.create!(user: user, entity: entity)
