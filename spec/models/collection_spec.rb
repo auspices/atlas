d2cb4ec3f8f0fc8fbe68b587133ffbe8bb605da0
dzucconi <mail@damonzucconi.com>
2020-04-15 10:07:12 -0400

    Cleans up associated content when deleting collections

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -22,4 +22,38 @@ RSpec.describe Collection, type: :model do
   it 'has a valid fabricator' do
     expect(collection).to be_valid
   end
+
+  describe 'relationships' do
+    let(:user) { Fabricate(:user) }
+
+    describe 'collections' do
+      let(:nested_collection) do
+        Fabricate(:collection).tap do |xs|
+          xs.contents.create!(user: user, entity: nested_text)
+        end
+      end
+
+      let(:nested_text) { Fabricate(:text) }
+      let(:text) { Fabricate(:text) }
+
+      it 'cleans up associated content' do
+        collection.contents.create!(user: user, entity: nested_collection)
+        collection.contents.create!(user: user, entity: nested_text)
+        collection.contents.create!(user: user, entity: text)
+
+        expect(collection.contents.size).to be(3)
+        expect(nested_collection.persisted?).to be(true)
+
+        nested_collection.destroy
+
+        collection.contents.reload
+        expect(collection.contents.size).to be(2)
+        expect(collection.contents.pluck(:entity_type)).to eql(['Text', 'Text'])
+
+        expect(nested_collection.persisted?).to be(false)
+        expect(nested_text.persisted?).to be(true)
+        expect(text.persisted?).to be(true)
+      end
+    end
+  end
 end
