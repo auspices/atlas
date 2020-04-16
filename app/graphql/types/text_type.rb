88850a53115b44a832fcc20ab61f89881b0241c7
dzucconi <mail@damonzucconi.com>
2020-04-16 08:38:38 -0400

    Exposes length field on texts

--- app/graphql/types/text_type.rb
+++ app/graphql/types/text_type.rb
@@ -8,5 +8,11 @@ module Types
     field :id, Int, null: false
     field :body, String, null: false
     field :name, String, null: false, method: :to_s
+
+    field :length, Int, null: false
+
+    def length
+      object.body.size
+    end
   end
 end
