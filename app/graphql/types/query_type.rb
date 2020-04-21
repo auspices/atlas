eb6e8cd59158f71b0718ad78a3409c77eade8a99
dzucconi <mail@damonzucconi.com>
2020-04-20 20:16:18 -0400

    Removes deprecated fields

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,29 +7,5 @@ module Types
     def me
       current_user
     end
-
-    field :user, Types::UserType, null: false, deprecation_reason: 'Moving all query underneath logged in user' do
-      argument :id, ID, required: true
-    end
-
-    def user(id:)
-      User.friendly.find(id)
-    end
-
-    field :collection, Types::CollectionType, null: false, deprecation_reason: 'Moving all query underneath logged in user' do
-      argument :id, ID, required: true
-    end
-
-    def collection(id:)
-      Collection.friendly.find(id)
-    end
-
-    field :content, Types::ContentType, null: false, deprecation_reason: 'Moving all query underneath logged in user' do
-      argument :id, ID, required: true
-    end
-
-    def content(id:)
-      Content.find(id)
-    end
   end
 end
