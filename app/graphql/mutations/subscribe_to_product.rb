af56c7b2b5913badf1fd8b4dc6cdf89c45f9cd32
dzucconi <mail@damonzucconi.com>
2020-08-02 16:15:29 -0400

    Wire up stripe to subscribe

--- app/graphql/mutations/subscribe_to_product.rb
+++ app/graphql/mutations/subscribe_to_product.rb
@@ -3,11 +3,28 @@
 module Mutations
   class SubscribeToProduct < BaseMutation
     argument :product, Types::ProductsType, required: true
+    argument :payment_method_id, String, required: true
+    argument :price_id, String, required: true
 
     field :user, Types::UserType, null: false
 
-    def resolve(product:)
-      current_user.subscribe_to!(product)
+    def resolve(product:, payment_method_id:, price_id:)
+      begin
+        Stripe::PaymentMethod.attach(payment_method_id, { customer: current_user.customer_id })
+
+        Stripe::Customer.update(current_user.customer_id,
+                                invoice_settings: { default_payment_method: payment_method_id })
+
+        subscription =
+          Stripe::Subscription.create(
+            customer: current_user.customer_id,
+            items: [{ price: price_id }] # TODO: Get the price selected?
+          )
+      rescue StandardError => e
+        return Errors::BadRequestError.new(e.message || e.error.message)
+      end
+
+      current_user.subscribe_to!(product) if subscription.status === 'active' || subscription.status === 'trialing'
 
       { user: current_user }
     end
