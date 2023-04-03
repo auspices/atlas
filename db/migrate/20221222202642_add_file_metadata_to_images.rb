c71de6de64537c9076dd3f90954351d78e11723b
dzucconi <mail@damonzucconi.com>
2023-04-03 14:58:33 -0400

    Rubocop autofixes

--- db/migrate/20221222202642_add_file_metadata_to_images.rb
+++ db/migrate/20221222202642_add_file_metadata_to_images.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddFileMetadataToImages < ActiveRecord::Migration[6.1]
   def change
     add_column :images, :file_name, :text
--- db/migrate/20230403021431_add_combined_index_to_contents.rb
+++ db/migrate/20230403021431_add_combined_index_to_contents.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddCombinedIndexToContents < ActiveRecord::Migration[6.1]
   def change
-    add_index :contents, [:collection_id, :entity_id, :entity_type]
+    add_index :contents, %i[collection_id entity_id entity_type]
   end
 end
