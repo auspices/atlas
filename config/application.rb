c7f4eb999bb6f2dba10d9a55e0f33b3a1e295d10
dzucconi <mail@damonzucconi.com>
2025-02-09 11:51:30 -0500

    Upgrades to Rails 7

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,14 @@ Bundler.require(*Rails.groups)
 module Atlas
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 7.1
 
-    config.assets.enabled = false
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     config.autoload_paths += %W[
-      #{config.root}/lib
       #{config.root}/app/services
     ]
 
@@ -36,5 +38,15 @@ module Atlas
     end
 
     config.middleware.use BatchLoader::Middleware
+
+    # For Rails 7.1: Configure your mailer to use Active Job
+    config.action_mailer.deliver_later_queue_name = :mailers
+    config.action_mailer.perform_deliveries = true
+
+    # For Rails 7.1: Set Time Zone
+    config.time_zone = 'UTC'
+
+    # Use credentials for secret key base
+    config.secret_key_base = credentials.secret_key_base
   end
 end
